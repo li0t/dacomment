f418c7fc6783b9e1d71abf248dd3f03722c3f4b8
li0t <leonardo0ramos@gmail.com>
2015-12-11 20:16:53 -0300

    Se arregla pequeña ruta de error

--- application/controllers/Documento.php
+++ application/controllers/Documento.php
@@ -24,8 +24,8 @@ class Documento extends CI_Controller {
 
   public function subir_documento($id_port=null)
   {
-    if (!$id_port)  show_404();
 
+    if (!$id_port) show_404();
     $this->layout->view('subir_documento', compact('id_port'));
 
   }
@@ -40,51 +40,54 @@ class Documento extends CI_Controller {
     $id_port =  $this->input->post("id_port",true);
     $id_doc = $this->input->post("id_doc",true);
 
+    if (!$id_port || !$nombreDocumento || !$usuario || !$descripcionDocumento) {
 
-    $fecha = date("Y-m-d H:i:s");
-
-    if (!$id_port || !$nombreDocumento || !$usuario || !$nombreDocumento || !$descripcionDocumento) show_404();
-
-
-    $carpetaPortafolio = './uploads/'.$id_port;
-    $carpetaDocumento = $carpetaPortafolio.'/'.$nombreDocumento;
-
-    if (!is_dir($carpetaPortafolio)) mkdir($carpetaPortafolio,0777);
-    if (!is_dir($carpetaDocumento)) mkdir($carpetaDocumento,0777);
-
-    $config['allowed_types'] = 'gif|jpg|png|doc|docx|pdf|txt';
-    $config['upload_path'] = $carpetaDocumento;
-    $config['file_name'] = $fecha; // El nombre del archivo corresponde a la fecha en la que fue subido.
-    $config['max_size'] = '10000';
-
-    $this->load->library('upload', $config);
-
-    if ($this->upload->do_upload())
-    {
-      if ($id_doc == "")
-      {
-        $data = array("PRO_ID"=>$id_port,"DOC_NOMBRE"=>$nombreDocumento, "DOC_FECHA"=>$fecha, "DOC_ESTADO"=>1, "ID_USUARIO"=>$usuario->USU_ID, "DOC_DESCRIPCION"=>$descripcionDocumento);
-        $this->documento_model->insertarDocumento($data);
-
-        $id_doc = $this->documento_model->obtenerIdDocumento($data);
-        $dataversion = array("DOC_ID"=>$id_doc->DOC_ID,"VER_NUMERO"=>"1", "VER_FECHA"=>date("Y-m-d H:i:s"),"ID_USUARIO"=>$usuario->USU_ID, "VER_COMENTARIO"=>$descripcionDocumento);
-        $this->version_model->insertarVersion($dataversion);
-        $this->session->set_flashdata("ControllerMessage","Se ha subido un nuevo documento!");
-        redirect(base_url()."portafolio/obtener_portafolio/".$id_port,301);
-      } else {
-        $ult_version = $this->version_model->obtenerUltimaVersion($id_doc);
-        $version = $ult_version->VER_NUMERO+1;
-        $dataversion = array("DOC_ID"=>$id_doc,"VER_NUMERO"=>$version, "VER_FECHA"=>date("Y-m-d H:i:s"),"ID_USUARIO"=>$usuario->USU_ID, "VER_COMENTARIO"=>$descripcionDocumento);
-        $this->version_model->insertarVersion($dataversion);
-        $this->session->set_flashdata("ControllerMessage","Se ha Actualizado una nueva Version!");
-        redirect(base_url()."documento/historia_documento/".$id_doc."/".$id_port,301);
-      }
+      $this->session->set_flashdata("ControllerMessage","Hay un error en los parámetros!");
+      redirect(base_url()."portafolio/obtener_portafolio/".$id_port,301);
 
     } else {
-      $this->session->set_flashdata("ControllerMessage","Ha habido un error subiendo el nuevo documento! ".$this->upload->display_errors());
-      redirect(base_url()."portafolio/obtener_portafolio/".$id,301);
-    }
 
+      $carpetaPortafolio = './uploads/'.$id_port;
+      $carpetaDocumento = $carpetaPortafolio.'/'.$nombreDocumento;
+
+      if (!is_dir($carpetaPortafolio)) mkdir($carpetaPortafolio,0777);
+      if (!is_dir($carpetaDocumento)) mkdir($carpetaDocumento,0777);
+
+      $fecha = date("Y-m-d H:i:s");
+
+      $config['allowed_types'] = 'gif|jpg|png|doc|docx|pdf|txt';
+      $config['upload_path'] = $carpetaDocumento;
+      $config['file_name'] = $fecha; // El nombre del archivo corresponde a la fecha en la que fue subido.
+      $config['max_size'] = '10000';
+
+      $this->load->library('upload', $config);
+
+      if ($this->upload->do_upload())
+      {
+        if ($id_doc == "")
+        {
+          $data = array("PRO_ID"=>$id_port,"DOC_NOMBRE"=>$nombreDocumento, "DOC_FECHA"=>$fecha, "DOC_ESTADO"=>1, "ID_USUARIO"=>$usuario->USU_ID, "DOC_DESCRIPCION"=>$descripcionDocumento);
+          $this->documento_model->insertarDocumento($data);
+
+          $id_doc = $this->documento_model->obtenerIdDocumento($data);
+          $dataversion = array("DOC_ID"=>$id_doc->DOC_ID,"VER_NUMERO"=>"1", "VER_FECHA"=>date("Y-m-d H:i:s"),"ID_USUARIO"=>$usuario->USU_ID, "VER_COMENTARIO"=>$descripcionDocumento);
+          $this->version_model->insertarVersion($dataversion);
+          $this->session->set_flashdata("ControllerMessage","Se ha subido un nuevo documento!");
+          redirect(base_url()."portafolio/obtener_portafolio/".$id_port,301);
+        } else {
+          $ult_version = $this->version_model->obtenerUltimaVersion($id_doc);
+          $version = $ult_version->VER_NUMERO+1;
+          $dataversion = array("DOC_ID"=>$id_doc,"VER_NUMERO"=>$version, "VER_FECHA"=>date("Y-m-d H:i:s"),"ID_USUARIO"=>$usuario->USU_ID, "VER_COMENTARIO"=>$descripcionDocumento);
+          $this->version_model->insertarVersion($dataversion);
+          $this->session->set_flashdata("ControllerMessage","Se ha Actualizado una nueva Version!");
+          redirect(base_url()."documento/historia_documento/".$id_doc."/".$id_port,301);
+        }
+
+      } else {
+        $this->session->set_flashdata("ControllerMessage","Ha habido un error subiendo el nuevo documento! ".$this->upload->display_errors());
+        redirect(base_url()."portafolio/obtener_portafolio/".$id_port,301);
+      }
+    }
   }
 
   public function versionar_documento($id_port=null,$id_doc=null)
