1f532656e8f883722b1e5904a6a3234afc7cb8c6
li0t <leonardo0ramos@gmail.com>
2015-12-14 11:57:52 -0300

    Fix vista de nuevo permiso por portafolio

--- application/views/portafolio/entregar_nuevo_permiso.php
+++ application/views/portafolio/entregar_nuevo_permiso.php
@@ -15,12 +15,12 @@ $atributos = array('id'=>'nuevopermiso','name'=>'nuevopermiso');
 echo form_open_multipart(null,$atributos);
 ?>
 <p>Nombre</p>
-<!--<input type="text" id="usuarioid" name="usuarioid" placeholder="Ingresa el ID del usuario a agregar!"/>-->  
+<input type="text" id="usuarioid" name="usuarioid" placeholder="Ingresa el ID del usuario a agregar!" required/>  
 <!-- Esto debería recibir un nombre -->
 <br>
 <select id="permisoid" name="permisoid">
 	<?php
-		foreach ($permisos as $per) 
+		foreach ($permisos as $per)
 		{
 	?>
 			<option value="<?php echo $per->PER_ID ?>"><?php echo $per->PER_DESCRIPCION ?></option>
